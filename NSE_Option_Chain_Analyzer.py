5f4a502acb1c455c524434ba8d7338dd3b83bfa7
VarunS2002 <30069380+varuns2002@users.noreply.github.com>
2026-05-23 01:15:57 +0530

    Workaround to fix crash by not accepting brotli encoded response from server

--- NSE_Option_Chain_Analyzer.py
+++ NSE_Option_Chain_Analyzer.py
@@ -58,7 +58,7 @@ class Nse:
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                           'Chrome/130.0.0.0 Safari/537.36',
             'accept-language': 'en,gu;q=0.9,hi;q=0.8',
-            'accept-encoding': 'gzip, deflate, br'}
+            'accept-encoding': 'gzip, deflate'}
         self.session: requests.Session = requests.Session()
         self.cookies: Dict[str, str] = {}
         self.get_symbols(window)
